b9aeb6d9fcc7e038607b825a3968d69bd17fdb92
Istvan Kiesz <kieszistvan@gmail.com>
2014-09-25 17:51:49 +0200

    back to top functionality

--- js/main.js
+++ js/main.js
@@ -1,76 +1,87 @@
-
 (function() {
-      new WOW( 
-        {
-          boxClass:     'wow',      
-          animateClass: 'animated', 
-          offset:       200 
-        }
-      ).init();
+  new WOW({
+    boxClass: 'wow',
+    animateClass: 'animated',
+    offset: 200
+  }).init();
 })();
 
-(function(){
-      // conversion tracking
-    $('.btn-submit').click(function(e) {
-      ga('send', 'event', 'subscribe', 'click');
-    });
+(function($) {
 
-    $('#counter').each(function(){
-      $(this).parallax('50%', 0.3, true);
-    });
+  // conversion tracking
+  $('.btn-submit').click(function(e) {
+    ga('send', 'event', 'subscribe', 'click');
+  });
 
-    $('#pitch').waypoint(function(direction) {
-      if (direction === 'down') {
-        $('#sidebar').show('slide');
-      } else {
-        $('#sidebar').hide('fast');
-      }
-    });
+  $('#counter').each(function() {
+    $(this).parallax('50%', 0.3, true);
+  });
+
+  $('#to-top').hide();
+  $('#sidebar').hide();
+
+  $('#pitch').waypoint(function(direction) {
+    if (direction === 'down') {
+      $('#sidebar').fadeIn();
+      $('#to-top').fadeIn();
+    } else {
+      $('#sidebar').fadeOut();
+      $('#to-top').fadeOut();
+    }
+  });
+
+  $('#to-top a').click(function() {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 2500);
+    return false;
+  });
+
 })(jQuery);
 
 
 (function($) {
 
-         // Smooth scroll for in page links
-          var target, scroll;
+  // Smooth scroll for in page links
+  var target, scroll;
 
-          $("a[href*=#]:not([href=#])").on("click", function(e) {
-            if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-              target = $(this.hash);
-              target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
+  $("a[href*=#]:not([href=#])").on("click", function(e) {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      target = $(this.hash);
+      target = target.length ? target : $("[id=" + this.hash.slice(1) + "]");
 
-              if (target.length) {
-                if (typeof document.body.style.transitionProperty === 'string') {
-                  e.preventDefault();
+      if (target.length) {
+        if (typeof document.body.style.transitionProperty === 'string') {
+          e.preventDefault();
 
-                  var avail = $(document).height() - $(window).height();
+          var avail = $(document).height() - $(window).height();
 
-                  scroll = target.offset().top;
+          scroll = target.offset().top;
 
-                  if (scroll > avail) {
-                    scroll = avail;
-                  }
+          if (scroll > avail) {
+            scroll = avail;
+          }
 
-                  $("html").css({
-                    "margin-top": ($(window).scrollTop() - scroll) + "px",
-                    "transition": "1s ease-in-out"
-                  }).data("transitioning", true);
-                } else {
-                  $("html, body").animate({
-                    scrollTop: scroll
-                  }, 1000);
-                  return;
-                }
-              }
-            }
-          });
+          $("html").css({
+            "margin-top": ($(window).scrollTop() - scroll) + "px",
+            "transition": "1s ease-in-out"
+          }).data("transitioning", true);
+        } else {
+          $("html, body").animate({
+            scrollTop: scroll
+          }, 1000);
+          return;
+        }
+      }
+    }
+  });
 
-          $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function(e) {
-            if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
-              $(this).removeAttr("style").data("transitioning", false);
-              $("html, body").scrollTop(scroll);
-              return;
-            }
-          });
+  $("html").on("transitionend webkitTransitionEnd msTransitionEnd oTransitionEnd", function(e) {
+    if (e.target == e.currentTarget && $(this).data("transitioning") === true) {
+      $(this).removeAttr("style").data("transitioning", false);
+      $("html, body").scrollTop(scroll);
+      return;
+    }
+  });
 
 })(jQuery);
